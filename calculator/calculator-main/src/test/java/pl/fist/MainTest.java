250f583daea1fe0faf22a0b6ca76a47b4d25d877
fistach <fistach@gmail.com>
2026-05-03 17:15:42 +0200

    add test for passing the strings to controller

--- calculator/calculator-main/src/test/java/pl/fist/MainTest.java
+++ calculator/calculator-main/src/test/java/pl/fist/MainTest.java
@@ -1,11 +1,13 @@
 package pl.fist;
 
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import java.io.PrintStream;
+import java.lang.reflect.Field;
 
 class MainTest {
 
@@ -16,10 +18,32 @@ class MainTest {
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
 
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
+        //when
+        Application.main(new String[]{});
+    }
+
+    @Test
+    void shouldPassReadStringsToController() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        String A = "5";
+        String B = "6";
+        String C = "+";
+
+        String input = A+"\n"+B+"\n"+C+"\nexit\n";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+
+        System.setIn(in);
+
+        DefaultController mockController = mock(DefaultController.class);
+
+        Field field = Application.class.getDeclaredField("controller");
+        field.setAccessible(true);
+        field.set(null, mockController);
 
         //when
         Application.main(new String[]{});
+
+        //then
+        verify(mockController).run(eq(A), eq(B), eq(C));
     }
 }
