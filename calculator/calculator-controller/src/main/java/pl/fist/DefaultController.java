fc84937e196db99853c18555bf64bf4a9965c5aa
fistach <fistach@gmail.com>
2026-05-03 15:11:50 +0200

    reject invalid input

--- calculator/calculator-controller/src/main/java/pl/fist/DefaultController.java
+++ calculator/calculator-controller/src/main/java/pl/fist/DefaultController.java
@@ -1,8 +1,24 @@
 package pl.fist;
 
+import java.util.Arrays;
+
 public class DefaultController {
 
-    public DefaultController() {
+    public DefaultController() {}
+
+    public void run(String a, String b, String c) {
+        parseNumber(a);
+        parseNumber(b);
+        if (!Arrays.asList("+", "-", "*", "/").contains(c)) {
+            throw new IllegalStateException(c + "is not a valid operation");
+        }
     }
 
+    private void parseNumber(String stringNumber) {
+        try {
+            Double.parseDouble(stringNumber);
+        } catch (NumberFormatException nfe) {
+            throw new IllegalStateException(stringNumber + "is not stringNumber number");
+        }
+    }
 }
