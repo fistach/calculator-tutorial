058f8253dc32585ba14c75be3e7c91f5f4c4921b
fistach <fistach@gmail.com>
2026-05-03 17:15:50 +0200

    pass the strings to controller

--- calculator/calculator-main/src/main/java/pl/fist/Application.java
+++ calculator/calculator-main/src/main/java/pl/fist/Application.java
@@ -4,13 +4,27 @@ import java.util.Scanner;
 
 public class Application {
 
+    private static CalculatorService service = new CalculatorService();
+    private static DefaultController controller = new DefaultController(service);
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int i = 0;
+        String[] strings = new String[3];
+
         while (true) {
             String line = scanner.nextLine();
+
             if ("exit".equalsIgnoreCase(line)) {
                 break;
+            }
+            strings[i] = line;
+            i++;
+
+            if (i == 3) {
+                controller.run(strings[0], strings[1], strings[2]);
+                i = 0;
             }
         }
     }
